d40d7009c991564c57454ef8c9e17c9a0af4b6fd
VDC-Digital <ptkrjf6b29@privaterelay.appleid.com>
2026-06-13 18:38:33 -0700

    Add VOODAC_CONTENT to content.js

---
+++ content.js
@@ -0,0 +1,24 @@
+const VOODAC_CONTENT = `
+
+[box2=S@N is back!]
+<bg=#fe0033>
+There is now more to your noon as <b>Sunday at Noon is back</b>. Expect first video updates starting 6.21.<br>
+<center><i>- Sunday at Noon Productions</i></center>
+</bg>
+[/box2]
+
+[video1=https://www.youtube.com/watch?v=SiQ44H9uGns]
+
+[box1=June - July Forecast]
+Thanks for visiting the Sunday at Noon Content Hub. Here at S@N, we want to provide a quality experience for you to catch up on the latest of news, whether it's at noon on a Sunday or any other time. <br>Any issues, please contact <link=mailto:nexus44.solder@icloud.com>S@N</link>.
+[/box1]
+[box2=Future Updates]
+Our goal is to release a shortcut for you to quickly open the Content Hub from your homescreen (on iPhone only).<br>
+We would also like to link other news articles that are worth a look.<br><br>
+
+Thats all!
+[/box2]
+
+[footer]Copyright © 2026 VOODAC Digital. VOODAC Digital is a trademark of VOODAC. | Canada[/footer] 
+
+`;
