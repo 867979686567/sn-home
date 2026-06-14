dfe5117210c3a9e1ce54b60730d7bdf5393d8f9c
VDC-Digital <ptkrjf6b29@privaterelay.appleid.com>
2026-06-13 20:21:56 -0700

    Update content.js

--- content.js
+++ content.js
@@ -7,7 +7,7 @@ There is now more to your noon as <b>Sunday at Noon is back</b>. Expect first vi
 </bg>
 [/box2]
 
-[video1=https://www.youtube.com/watch?v=SiQ44H9uGns]
+[video1=https://youtu.be/SiQ44H9uGns]
 
 [box1=June - July Forecast]
 Thanks for visiting the Sunday at Noon Content Hub. Here at S@N, we want to provide a quality experience for you to catch up on the latest of news, whether it's at noon on a Sunday or any other time. <br>Any issues, please contact <link=mailto:nexus44.solder@icloud.com>S@N</link>.
